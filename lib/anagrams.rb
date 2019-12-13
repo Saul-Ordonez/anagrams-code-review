e93f7dcc37b1eb9b2785c680f736c7c4a01022a6
Saul Ordonez <sauleordonez@gmail.com>
2019-12-13 14:30:09 -0800

    add script file for terminal accessibility

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 class Anagrams
   attr_reader(:input_one, :input_two)
 
@@ -9,9 +10,9 @@ class Anagrams
   def anagram_check
     first_word = @input_one.gsub(/\s+/, '').downcase.chars.sort.join
     second_word = @input_two.gsub(/\s+/, '').downcase.chars.sort.join
-    first_vowels = first_word.scan(/[aeoui]/)
-    second_vowels = second_word.scan(/[aeoui]/)
-    if first_vowels.length == 0 || second_vowels.length == 0
+    first_word_vowels = first_word.scan(/[aeoui]/)
+    second_word_vowels = second_word.scan(/[aeoui]/)
+    if first_word_vowels.length == 0 || second_word_vowels.length == 0
       outcome = 'Please enter a real word'
     elsif first_word == second_word
       outcome = 'These words are anagrams'
---
+++ script.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require ('./lib/anagrams.rb')
+
+puts "Enter a word:"
+@input_one = gets.chomp
+puts "Enter another word:"
+@input_two = gets.chomp
+
+anagrams = Anagrams.new(@input_one, @input_two)
+outcome = anagrams.anagram_check()
+
+
+if outcome == 'Please enter a real word'
+  puts 'Please enter a real word'
+elsif outcome == 'These words are anagrams'
+  puts 'These words are anagrams'
+else
+  puts 'These words are not anagrams'
+end
